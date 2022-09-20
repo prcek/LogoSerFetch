f4253dcbae01e1aaf23327f51a1cdce4f6f26984
Tomas Hluchan <tomas.hluchan@gmail.com>
2022-09-20 12:24:02 +0200

    opts sync

--- main.go
+++ main.go
@@ -52,6 +52,20 @@ type LogoMeta struct {
 	Note         string
 }
 
+type LogoExtendMeta struct {
+	Tag            string
+	S3KeySrc       string
+	S3KeyCandidate string
+	SeqNo          int
+	Merchant_id    int64
+	Color          string
+	Disable        bool
+	Keep           bool
+	Space          bool
+	Zoom           bool
+	HardCrop       bool
+}
+
 type FileElem struct {
 	name        string
 	s3key       string
@@ -74,6 +88,25 @@ type BatchElem struct {
 	files         *map[string]*FileElem
 }
 
+type FileOptionElem struct {
+	Merchant_id    int64
+	srcS3Key       string
+	candidateS3Key string
+	color          string
+	disable        bool
+	keep           bool
+	space          bool
+	zoom           bool
+	hardcrop       bool
+}
+
+type BatchOptionElem struct {
+	err           string
+	name          string
+	metafileS3key string
+	fileOpts      *map[int64]*FileOptionElem
+}
+
 func S3FileRead(svc *s3.S3, s3key string) ([]byte, error) {
 	requestInput := s3.GetObjectInput{
 		Bucket: aws.String(os.Getenv("AWS_S3_BUCKET")),
@@ -92,27 +125,13 @@ func S3FileRead(svc *s3.S3, s3key string) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func S3Fetch() map[string]*BatchElem {
-	aws_s3_bucket := os.Getenv("AWS_S3_BUCKET")
-	log.Print("AWS_S3_BUCKET is ", aws_s3_bucket)
-	aws_s3_region := os.Getenv("AWS_S3_REGION")
-	log.Print("AWS_S3_REGION is ", aws_s3_region)
-	aws_access_key_id := os.Getenv("AWS_ACCESS_KEY_ID")
-	log.Print("AWS_ACCESS_KEY_ID is ", aws_access_key_id)
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(os.Getenv("AWS_S3_REGION"))},
-	)
-	if err != nil {
-		log.Fatal("Error new sessiong", err)
-	}
-	svc := s3.New(sess)
+func S3Fetch(svc *s3.S3) map[string]*BatchElem {
 
 	bucket := os.Getenv("AWS_S3_BUCKET") //"dateio-logoser"
 
 	delim := ""
-	prefix := "logos_to_process/"
-	//prefix := "logos_to_process/logos_2022-09-07"
+	//prefix := "logos_to_process/"
+	prefix := "logos_to_process/logos_2022-09"
 	maxkey := int64(1000)
 
 	input := s3.ListObjectsV2Input{
@@ -127,7 +146,7 @@ func S3Fetch() map[string]*BatchElem {
 
 	batches := make(map[string]*BatchElem)
 
-	err = svc.ListObjectsV2Pages(&input, func(objs *s3.ListObjectsV2Output, b bool) bool {
+	err := svc.ListObjectsV2Pages(&input, func(objs *s3.ListObjectsV2Output, b bool) bool {
 		log.Print("s3 page loaded - ", page)
 
 		for _, item := range objs.Contents {
@@ -272,6 +291,95 @@ func S3Fetch() map[string]*BatchElem {
 	return batches
 }
 
+func S3FetchOptions(svc *s3.S3) map[string]*BatchOptionElem {
+
+	bucket := os.Getenv("AWS_S3_BUCKET") //"dateio-logoser"
+
+	delim := ""
+	prefix := "logos_options/logos_2022-09"
+	//prefix := "logos_options/logos_2022-09-07"
+	maxkey := int64(1000)
+
+	input := s3.ListObjectsV2Input{
+		Bucket: &bucket,
+
+		Delimiter: &delim,
+
+		MaxKeys: &maxkey,
+		Prefix:  &prefix,
+	}
+	page := 1
+
+	batches := make(map[string]*BatchOptionElem)
+
+	err := svc.ListObjectsV2Pages(&input, func(objs *s3.ListObjectsV2Output, b bool) bool {
+		log.Print("s3 page loaded - ", page)
+
+		for _, item := range objs.Contents {
+			//fmt.Println("key size", *item.Key, *item.Size)
+			parts := strings.Split(*item.Key, "/")
+			if len(parts) > 2 {
+				batch_id := parts[1]
+
+				be := batches[batch_id]
+				if be == nil {
+					be = new(BatchOptionElem)
+					fl := make(map[int64]*FileOptionElem)
+					be.fileOpts = &fl
+					be.name = batch_id
+					batches[batch_id] = be
+				}
+
+				file_name := parts[2]
+				if file_name == "meta_v1.json" {
+					be.metafileS3key = *item.Key
+					data, err := S3FileRead(svc, be.metafileS3key)
+					if err != nil {
+						log.Panic("can't fetch options metafile ", be.metafileS3key)
+					}
+					var lms []LogoExtendMeta
+					err = json.Unmarshal(data, &lms)
+					if err != nil {
+						log.Panic("can't parse json", be.metafileS3key)
+					}
+					for _, lm := range lms {
+						//fmt.Println(lm.New_filename)
+						fe := (*be.fileOpts)[lm.Merchant_id]
+						if fe != nil {
+							if !fe.disable { // not disabled => do not overrire
+								log.Print("duplicate opt for ", lm.Merchant_id, " keep current")
+								continue
+							}
+							log.Print("duplicate opt for ", lm.Merchant_id, " override")
+						} else {
+							fe = new(FileOptionElem)
+							(*be.fileOpts)[lm.Merchant_id] = fe
+						}
+						fe.color = lm.Color
+						fe.disable = lm.Disable
+						fe.candidateS3Key = lm.S3KeyCandidate
+						fe.srcS3Key = lm.S3KeySrc
+						fe.hardcrop = lm.HardCrop
+						fe.keep = lm.Keep
+						fe.space = lm.Space
+						fe.zoom = lm.Zoom
+
+					}
+
+				}
+
+			}
+		}
+
+		page++
+		return true
+	})
+	if err != nil {
+		log.Fatal("Error ListObjects", err)
+	}
+	return batches
+}
+
 func createFireStoreClient(ctx context.Context) *firestore.Client {
 	// Sets your Google Cloud Platform project ID.
 	projectID := os.Getenv("GCP_PROJECT")
@@ -285,11 +393,7 @@ func createFireStoreClient(ctx context.Context) *firestore.Client {
 	return client
 }
 
-func FireStoreUpdate(batches map[string]*BatchElem) {
-
-	ctx := context.Background()
-	client := createFireStoreClient(ctx)
-	defer client.Close()
+func FireStoreUpdate(client *firestore.Client, ctx context.Context, batches map[string]*BatchElem) {
 
 	iter := client.Collection("logo_batches").Documents(ctx)
 	for {
@@ -357,6 +461,70 @@ func FireStoreUpdate(batches map[string]*BatchElem) {
 
 }
 
+func FireStoreUpdateOpts(client *firestore.Client, ctx context.Context, batches map[string]*BatchOptionElem) {
+	iter := client.Collection("logo_batches").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		bid := doc.Ref.ID
+		batch := batches[bid]
+		if batch == nil {
+			log.Print("missing S3 batch ", bid)
+		}
+	}
+
+	for bkey, batch := range batches {
+
+		iter := client.Collection("logo_batches").Doc(bkey).Collection("logo_files").Documents(ctx)
+		for {
+			doc, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Failed to iterate: %v", err)
+			}
+			fid := doc.Ref.ID
+			type FileE struct {
+				MerchantId int64 `firestore:"merchantId"`
+			}
+
+			var fex FileE
+			err = doc.DataTo(&fex)
+			if err != nil {
+				log.Panic("can't read logo_files document")
+			}
+			fo := (*batch.fileOpts)[fex.MerchantId]
+			if fo == nil {
+				log.Print("missing S3 fileopt ", fex.MerchantId, " ", fid)
+				continue
+			}
+
+			_, err = client.Collection("logo_batches").Doc(bkey).Collection("logo_opts").Doc(fid).Set(ctx, map[string]interface{}{
+				"merchantId":     fex.MerchantId,
+				"color":          fo.color,
+				"disable":        fo.disable,
+				"keep":           fo.keep,
+				"space":          fo.space,
+				"zoom":           fo.zoom,
+				"hardcrop":       fo.hardcrop,
+				"candidateS3Key": fo.candidateS3Key,
+				"srcS3Key":       fo.srcS3Key,
+			})
+			if err != nil {
+				log.Fatalf("Failed adding file: %v", err)
+			}
+
+		}
+
+	}
+}
+
 func main() {
 	log.Print("LogoSerFetch 0.1")
 
@@ -370,9 +538,32 @@ func main() {
 	} else {
 		log.Print("Running in container. Using env.")
 	}
-	batches := S3Fetch()
 
-	FireStoreUpdate(batches)
-	//parseBatch("logos_2022-09-06T16:11:40Z")
+	ctx := context.Background()
+	fsClient := createFireStoreClient(ctx)
+	defer fsClient.Close()
+
+	aws_s3_bucket := os.Getenv("AWS_S3_BUCKET")
+	log.Print("AWS_S3_BUCKET is ", aws_s3_bucket)
+	aws_s3_region := os.Getenv("AWS_S3_REGION")
+	log.Print("AWS_S3_REGION is ", aws_s3_region)
+	aws_access_key_id := os.Getenv("AWS_ACCESS_KEY_ID")
+	log.Print("AWS_ACCESS_KEY_ID is ", aws_access_key_id)
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("AWS_S3_REGION"))},
+	)
+	if err != nil {
+		log.Fatal("Error new sessiong", err)
+	}
+	svc := s3.New(sess)
+
+	batches := S3Fetch(svc)
+
+	FireStoreUpdate(fsClient, ctx, batches)
+
+	batchesOpt := S3FetchOptions(svc)
+	FireStoreUpdateOpts(fsClient, ctx, batchesOpt)
+
 	log.Print("Done.")
 }
